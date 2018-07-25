62eef301e5365f3a278862029070510ef6c09702
yokeshthirumoorthi <yokeshthirumoorthi@gmail.com>
2018-07-25 07:20:19 -0700

    Add quill editor component

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Chatwindow from './Chatwindow';
+import QuillEditor from './QuillEditor';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <QuillEditor />
         <Chatwindow />
       </div>
     );
---
+++ web/src/QuillEditor.js
@@ -0,0 +1,46 @@
+/* 
+ * Simple editor component that takes placeholder text as a prop 
+ */
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import ReactQuill from 'react-quill'
+
+class QuillEditor extends Component {
+    constructor(props) {
+        super(props)
+        this.quillRef = null;      // Quill instance
+        this.reactQuillRef = null; // ReactQuill component
+    }
+
+    componentDidMount() {
+        this.attachQuillRefs()
+    }
+
+    componentDidUpdate() {
+        this.attachQuillRefs()
+    }
+
+    attachQuillRefs = () => {
+        if (typeof this.reactQuillRef.getEditor !== 'function') return;
+        this.quillRef = this.reactQuillRef.getEditor();
+    }
+
+    insertText = () => {
+        var range = this.quillRef.getSelection();
+        let position = range ? range.index : 0;
+        this.quillRef.insertText(position, 'Hello, World! ')
+    }
+
+    render() {
+        return (
+            <div>
+                <ReactQuill
+                    ref={(el) => { this.reactQuillRef = el }}
+                    theme={'snow'} />
+                <button onClick={this.insertText}>Insert Text</button>
+            </div>
+        )
+    }
+}
+
+export default QuillEditor;
