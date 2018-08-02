8bc86ee1739001ea310e8aac7e4acb91a1360464
yokeshthirumoorthi <yokeshthirumoorthi@gmail.com>
2018-08-02 08:37:13 -0700

    add copyright in web files

--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,8 @@
+// Copyright © 2018 Yokesh Thirumoorthi
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
